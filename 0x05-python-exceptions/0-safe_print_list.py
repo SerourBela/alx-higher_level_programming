6a2b27fb35bb2a4edb9a210da59ad2e6da25cf0b
SerourBela <serour.belabed@univ-constantine2.dz>
2023-12-18 09:37:11 -0800

    update

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    a = 0
-    for b in range(x):
+    count = 0
+
+    for i in range(x):
         try:
-            print("{}".format(my_list[b]), end="")
-            a += 1
-        except IndexError:
+            print("{}".format(my_list[i]), end='')
+        except:
             break
-    print("")
-    return(a)
+        else:
+            count += 1
+
+    print()
+    return (count)
--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
-def safe_function(fct, *args):
-    import sys
-    try:
-        result = fct(*args)
-    except Exception as i:
-        sys.stderr.write("Exception: {}\n".format(i))
-        result = None
-
-    return (result)
+def magic_calculation(a, b):
+    result = 0
+    for i in range(1, 3):
+        try:
+            if i > a:
+                raise Exception('Too far')
+            else:
+                result += a ** b / i
+        except Exception:
+            result = b + a
+            break
+    return result

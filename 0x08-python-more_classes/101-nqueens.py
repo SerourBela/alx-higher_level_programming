392badf400dcdd435351e23b46f687edd18bad69
SerourBela <serour.belabed@univ-constantine2.dz>
2024-01-04 03:54:29 -0800

    update

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,70 +1,63 @@
 #!/usr/bin/python3
+from sys import argv
+
 """
-Resolve NQUEEN Problem
+This modules finds all solutions for N queens problem
 """
 
 
-def check(queen, column):
+class Queen:
     """
-    function that checks if the position of each queen is valid
+    Class defined as Queen to solve nQueens problem
+    using recursion
     """
-    for i in range(column):
-        if queen[i] == queen[column]:
-            return False
-        if abs(queen[i] - queen[column]) == abs(i - column):
-            return False
-    return True
+    def can_move(self, x, y, right):
 
+        """
+        Function to see if queen can move in the vaild constraint
+        column provided
+        """
+        for a in range(x):
+            if right[a] == y:
+                return (False)
+            if abs(right[a] - y) == (x - a):
+                return (False)
+        return (True)
 
-def fulling(queen, column):
-    """
-    Recursive Function that change the queen when we
-    get the correct position with no problems
-    """
+    def solution(self, n, N, right):
+        """
+        function to find all the right combos that can happen
+        using recursion.
+        """
+        if n == N:
+            print("[", end="")
+            for j in range(N):
+                print("[{}, {}]".format(j, right[j]), end="")
+                if j < N - 1:
+                    print(", ", end="")
+            print("]")
+            return
 
-    tam = len(queen)
-    exito = 0
-
-    if column == tam:
-        result = []
-
-        for i in range(len(queen)):
-            result.append([i, queen[i]])
-
-        print(result)
-        return True
-
-    queen[column] = -1
-
-    while(queen[column] < tam - 1 or exito == 1):
-        queen[column] = queen[column] + 1
-        if check(queen, column) is True:
-            if column != tam:
-                fulling(queen, (column + 1))
-            else:
-                exito = 1
-                break
-    return True
-
+        for j in range(N):
+            if self.can_move(n, j, right):
+                right[n] = j
+                self.solution(n + 1, N, right)
 
 if __name__ == "__main__":
-    import sys
+    count = len(argv)
 
-    if len(sys.argv) != 2:
+    if count != 2:
         print("Usage: nqueens N")
-        sys.exit(1)
-
-    if not sys.argv[1].isdigit():
-        print("N must be a number")
-        sys.exit(1)
-
-    if int(sys.argv[1]) < 4:
+        exit(1)
+    else:
+        try:
+            N = int(argv[1])
+        except:
+            print("N must be a number")
+            exit(1)
+    if N < 4:
         print("N must be at least 4")
-        sys.exit(1)
+        exit(1)
 
-    queen = []
-    tam = int(sys.argv[1])
-    for i in range(tam):
-        queen.append(-1)
-
-    fulling(queen, 0)
+    final = Queen()
+    final.solution(0, N, [None for i in range(N)])
